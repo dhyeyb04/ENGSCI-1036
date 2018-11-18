4d778782e66dadd6cb35da8bbd068f545ce0a463
dhyey <dhyey@172.30.229.56>
2018-11-18 18:37:47 -0500

    Completed Lab 06 Q1

--- src/dbhavsa2_lab06/dbhavsa2_lab06_q1.java
+++ src/dbhavsa2_lab06/dbhavsa2_lab06_q1.java
@@ -5,103 +5,95 @@ import java.util.InputMismatchException;
 
 public class dbhavsa2_lab06_q1 {
 	static class ConsoleInput {
-		//static Scanner scanner = new Scanner(System.in);
+		// static Scanner scanner = new Scanner(System.in);
 		public static int getValidatedInteger(int minValue, int maxValue) {
 			Scanner intscanner = new Scanner(System.in);
 			int i = 0;
-			//boolean correct = false;
-			
+			boolean correct = false;
+
 			do {
 				try {
 					System.out.print("Please input an integer between 4 and 19 inclusive: ");
 					i = intscanner.nextInt();
-					
+
 					if (i > maxValue || i < minValue) {
 						System.out.println("Incorrect range!");
 					}
-					//else if (i >= minValue && i <= maxValue) {
-					//	correct = true;
-					//}
-				}
-				catch (InputMismatchException e) {
+					else if (i >= minValue && i <= maxValue) {
+					 correct = true;
+					}
+				} catch (InputMismatchException e) {
 					System.out.println("Incorrect format!");
 					intscanner.nextLine(); // This is used to clear console
-					
+
 				}
-				/*finally {
-					if (correct == true) {
-						intscanner.close();	
-					}
-				}*/
-			} while (i > maxValue || i < minValue);
+				/*
+				 * finally { if (correct == true) { intscanner.close(); } }
+				 */
+			} while (correct == false);
 			return i;
 		}
 
 		public static double getValidatedDouble(double minValue, double maxValue) {
 			Scanner doublescanner = new Scanner(System.in);
 			double d = 0;
-			//boolean correct = false;
-			
+			boolean correct = false;
+
 			do {
 				try {
 					System.out.print("Please input a double between -34.6 and 85.1 inclusive: ");
 					d = doublescanner.nextDouble();
-					
+
 					if (d > maxValue || d < minValue) {
 						System.out.println("Incorrect range!");
+					} else if (d >= minValue && d <= maxValue) {
+						correct = true;
 					}
-					//else if (d >= minValue && d <= maxValue) {
-					//	correct = true;
-					//}
-				}
-				catch (InputMismatchException e) {
+				} catch (InputMismatchException e) {
 					System.out.println("Incorrect format!");
 					doublescanner.nextLine(); // This is used to clear console
-					
+
 				}
-				/*finally {
-					if (correct == true) {
-						doublescanner.close();	
-					}
-				}*/
-			} while (d > maxValue || d < minValue);
+				/*
+				 * finally { if (correct == true) { doublescanner.close(); } }
+				 */
+			} while (correct == false);
 			return d;
 		}
 
 		public static char getValidatedChar(char minChar, char maxChar) {
 			Scanner charscanner = new Scanner(System.in);
 			char c = ' ';
-			//boolean correct = false;
-			
+			boolean correct = false;
+
 			do {
 				try {
-					System.out.print("Please input a character between " + minChar + " and " + maxChar + " inclusive: ");
+					System.out
+							.print("Please input a character between " + minChar + " and " + maxChar + " inclusive: ");
 					c = charscanner.next().charAt(0);
-					
+
 					if (c > maxChar || c < minChar) {
 						System.out.println("Incorrect range!");
+					} else if (c >= minChar && c <= maxChar) {
+						correct = true;
 					}
-					//else if (c >= minChar && c <= maxChar) {
-					//	correct = true;
-					//}
-				}
-				catch (InputMismatchException e) {
+				} catch (InputMismatchException e) {
 					System.out.println("Incorrect format!");
 					charscanner.next(); // This is used to clear console
 				}
-				//finally {
-				//	if (correct == true) {
-				//		charscanner.close();
-				//	}
-				//}
-			} while (c > maxChar || c < minChar);
+				// finally {
+				// if (correct == true) {
+				// charscanner.close();
+				// }
+				// }
+			} while (correct == false);
 			return c;
 		}
 
 	}
 
 	public static void main(String[] args) {
-		ConsoleInput.getValidatedInteger(4 , 19);
+		ConsoleInput.getValidatedInteger(4, 19);
 		ConsoleInput.getValidatedDouble(-34.6, 85.1);
 		ConsoleInput.getValidatedChar('a', 'f');
 	}
